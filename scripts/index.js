9a0be55c016b5cfc319d80a8084add2fc1eafb27
Nina Grishina <nino44ka.kalinkina@yandex.ru>
2025-01-11 22:20:59 +0300

    update #1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard (card, deleteCard) {
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+cardElement.querySelector('.card__image').src = card.link;
+cardElement.querySelector('.card__image').alt = card.alt;
+cardElement.querySelector('.card__title').textContent = card.name;
+const removeCard = cardElement.querySelector('.card__delete-button');
+removeCard.addEventListener('click', function(){
+    removeCard(card)});
+return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+    renderCard(createCard('.card'))
+})
