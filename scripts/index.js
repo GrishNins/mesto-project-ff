e5ee8b68beca21ca59e744ac762fcfa7fcd16eb9
Nina Grishina <nino44ka.kalinkina@yandex.ru>
2025-01-12 11:29:26 +0300

    update 1

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,22 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard (card, deleteCard) {
-const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-cardElement.querySelector('.card__image').src = card.link;
-cardElement.querySelector('.card__image').alt = card.alt;
-cardElement.querySelector('.card__title').textContent = card.name;
-const removeCard = cardElement.querySelector('.card__delete-button');
-deleteCard.addEventListener('click', function(){
-    removeCard(card)});
-return cardElement;
+function createCard (card, deleteButton) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.alt;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click',() => deleteCard(cardElement));
+  return cardElement;
 }
 
 // @todo: Функция удаления карточки
-
+ function deleteCard() {
+    remove(card);
+ }
 
 // @todo: Вывести карточки на страницу
+card.forEach(function(element) {
+placesList.append(cardElement);
+})
